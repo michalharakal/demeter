44466b4bc26da95b155ea746583d46ee4a739477
Michal Harakal <michal.harakal@gmail.com>
2017-04-05 11:30:17 +0200

    - manualna verzia, CML zapojeny:   - simple demeter REST API

---
+++ cml/app/src/main/java/com/fiwio/iot/demeter/api/Relay.java
@@ -0,0 +1,32 @@
+
+package com.fiwio.iot.demeter.api;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Relay {
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("value")
+    @Expose
+    private String value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+}
