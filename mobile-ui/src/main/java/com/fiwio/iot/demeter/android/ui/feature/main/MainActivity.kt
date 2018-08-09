6e4b0a44f7ef4594cb18ea083eb7e1c677cdc6ae
Michal Harakal <michal.harakal@gmail.com>
2018-08-09 21:27:02 +0200

    adds receiver for firebase messages (no UI)

--- mobile-ui/src/main/java/com/fiwio/iot/demeter/android/ui/feature/main/MainActivity.kt
+++ mobile-ui/src/main/java/com/fiwio/iot/demeter/android/ui/feature/main/MainActivity.kt
@@ -3,8 +3,11 @@ package com.fiwio.iot.demeter.android.ui.feature.main
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.support.annotation.NonNull
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import com.fiwio.iot.demeter.android.ui.R
 import com.fiwio.iot.demeter.android.ui.app.EndpointUrlProvider
@@ -15,6 +18,11 @@ import com.fiwio.iot.demeter.android.ui.feature.manual.ManualControlView
 import com.fiwio.iot.demeter.android.ui.feature.refresh.RefreshIntentService
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.messaging.FirebaseMessaging
+
 
 class MainActivity : AppCompatActivity(), AutomaticNavigator {
 
@@ -58,6 +66,19 @@ class MainActivity : AppCompatActivity(), AutomaticNavigator {
         endpointUrlProvider.url = intent.getStringExtra("url")
 
         showView(R.id.navigation_manual)
+
+        FirebaseMessaging.getInstance().subscribeToTopic("events")
+                .addOnCompleteListener(object : OnCompleteListener<Void> {
+                    override fun onComplete(@NonNull task: Task<Void>) {
+                        var msg = getString(R.string.msg_subscribed)
+                        if (!task.isSuccessful()) {
+                            msg = getString(R.string.msg_subscribe_failed)
+                        }
+                        Log.d("FIRE", msg)
+                        Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+                    }
+                })
+
     }
 
     override fun onResume() {
